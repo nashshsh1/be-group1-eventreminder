fac65eed46c76dc51a3f016c72b791a38121bc41
nashshhshh <151946783+nashshsh1@users.noreply.github.com>
2024-11-15 22:27:47 +0800

    UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,11 +9,7 @@ class UserController extends Controller
 {
     public function index(Request $request)
     {
-        // Use the paginate method to paginate users, with a default of 10 users per page
-        $users = User::paginate(10);
-
-        // Return the paginated response
-        return response()->json($users);
+        //
     }
 
     /**
