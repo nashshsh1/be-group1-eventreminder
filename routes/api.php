514b085c7d68fd9f86d689de1a65a1cf5f5f2f1f
nashshhshh <151946783+nashshsh1@users.noreply.github.com>
2024-11-23 01:10:22 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::post('/register', [AuthController::class, 'register']);
+Route::get('/users', [UserController::class, 'user']);
